9badf6c4e7f98c3d7e0915848092b10ae2e30fae
Samifide <samiperez0311@gmail.com>
2024-03-07 07:36:04 -0600

    opción 3 visualización edición

--- codigo.py
+++ codigo.py
@@ -92,26 +92,8 @@ def menu_administrador():
 
         elif opc == "3":
             print("Visualizar vehículos")
-            reservas_activas = { "reserva1": {"cliente": "Juan", "monto": 100}, "reserva2": {"cliente": "Maria", "monto": 150}, "reserva3": {"cliente": "Pedro", "monto": 200} }
-
-            for reserva, datos in reservas_activas.items():
-                print(f"Reserva: {reserva} - Cliente: {datos['cliente']} - Monto: ${datos['monto']}")
-
-            reserva_cancelar = "reserva2"
-
-            if reserva_cancelar in reservas_activas: 
-                monto_reserva = reservas_activas[reserva_cancelar]["monto"] 
-                penalidad = monto_reserva * 0.10
-
-                print(f"La penalidad por cancelar la reserva es de ${penalidad}")
-                confirmacion = input("¿Desea cancelar la reserva? (s/n): ")
-
-                if confirmacion.lower() == "s":
-                    del reservas_activas[reserva_cancelar]
-                    print(f"La reserva {reserva_cancelar} ha sido cancelada")
-                else:
-                    print("La reserva se mantendrá activa")
-            else: 
-                print("La reserva ingresada no existe")
+            for i in inventario_vehiculos:
+                print(i)
+            print("Estos son los vehiculos")
 
 menu_administrador()
